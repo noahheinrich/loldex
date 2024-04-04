72cb774ef77e6c4482caab533dcd3067e4cce5c8
noahheinrich <noahheinrich38@gmail.com>
2024-04-04 11:08:30 +0200

    service worker

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
+import { createRoot } from 'react-dom/client';
 import React from 'react';
-import ReactDOM from 'react-dom';
-//import './index.css'; // Assurez-vous d'avoir ce fichier pour les styles globaux
 import App from './App'; // Le composant racine de votre application
-//import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Généré par Create React App pour la PWA
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
+
+const container = document.getElementById('root'); // Assurez-vous que cela correspond à l'ID de votre élément racine dans le HTML
+const root = createRoot(container); // Créez la racine à partir du conteneur
 
 // Rendu de l'application React dans l'élément racine du DOM
-ReactDOM.render(
+root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
